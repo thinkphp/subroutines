b2691796c1c64d18b312bc805f5f11943997f67d
Adrian Statescu <mergesortv@gmail.com>
2021-06-07 10:24:55 +0300

    Create palindrom.cpp

---
+++ palindrom.cpp
@@ -0,0 +1,37 @@
+/*
+ Se citesc doua numere naturale m < n; Se cere sa se tipareasca toate numerele palindrom 
+ aflate intre m si n. Un numar este palindrom daca citit diresc si invers rezultatul este acelasi.
+ */
+#include <iostream>
+
+using namespace std;
+
+int isPalindrom(int n) {
+	
+	int m = n, s = 0;
+	
+	while(n) {
+	   s = s * 10 + n % 10;
+	   n = n / 10;
+	}
+	
+	if(s == m) {
+	   return 1;	
+	} else {
+	   return 0;	
+	}
+}
+
+int main(void) {
+	int m, n;
+	
+	cout<<"m = "<<endl<<"n = ";
+	cin>>m>>n;
+	for(int i = m; i <= n; ++i) {
+		
+		if(isPalindrom( i )) {
+		   cout<<i<<endl;	
+		}
+	}
+ 
+}
